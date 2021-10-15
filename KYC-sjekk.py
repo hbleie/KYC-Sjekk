b6417eee5cc91d3987e3f01e289841c00d78a5d6
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-15 16:38:30 +0200

    søk i selskap og søk i roller i et selskap

--- KYC-sjekk.py
+++ KYC-sjekk.py
@@ -1,46 +1,54 @@
 import streamlit as st
 import requests
 
-st.title("Know Your Customer. PEP-sjekk")
+
 
 filter = st.sidebar.selectbox("Søk etter", ["Personer", "Selskap", "Roller i et selskap"])
+st.title(f"Know Your Customer - {filter}")
 
 payload={}
 headers = {}
 
+def PepSøk(input):
+    url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
+    response = requests.request("GET", url, headers=headers, data=payload)
+    r = response.json()
+    st.write('Number of hits: ' + str(r['numberOfHits']))
+    st.header('Political Exposed Person = ' + str(r['numberOfHits'] > 0))
+    for hit in r["hits"]:
+        st.header(hit['name'])
+        st.write('BIRTH DATE: ' + hit['birth_date'])
+        st.write('FOUND IN DATASET: ' + hit['dataset'])
+        st.write('ALIASES: ' + hit['aliases'])
+        st.write('COUNTRY: ' + hit['countries'])
+
 if filter == "Personer":
+   
     input = st.text_input("Søk etter person")
     if input:
-        url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
-
-        response = requests.request("GET", url, headers=headers, data=payload)
-        r = response.json()
-        st.header('Number of hits: ' + str(r['numberOfHits']))
-        for hit in r["hits"]:
-            st.header(hit['name'])
-            st.write('Birth Date: ' + hit['birth_date'])
-            st.write('Found in dataset: \n' + hit['dataset'])
-            st.write('Aliases: \n' + hit['aliases'])
-            st.write('Country: \n' + hit['countries'])
-        st.header('Political Exposed Person = ' + str(r['numberOfHits'] > 0))
-
-            
-
+        PepSøk(input)
+    
 
 
 if filter == "Selskap":
+   
     input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
-
 
     if input:
         url = "https://stacc-code-challenge-2021.azurewebsites.net/api/enheter?orgNr=" + input
 
         response = requests.request("GET", url, headers=headers, data=payload)
+        r = response.json()
+        st.header(r['navn'])
+        st.write('Organisasjonsform: ' + r['organisasjonsform']['beskrivelse'])
+        st.write('Stiftelsesdato: ' + r["stiftelsesdato"])
+        st.write('Beskrivelse: ' + r["naeringskode1"]['beskrivelse'])
+        st.write('Antall ansatte: ' + str(r['antallAnsatte']))
+        st.write('Forretningsadresse: ' + r['forretningsadresse']['adresse'][0] + ', ' + r['forretningsadresse']['postnummer'] + ', ' + r['forretningsadresse']['poststed'] + ', ' + r['forretningsadresse']['land'])
 
-        st.write(response.json())
-        
 
 if filter == "Roller i et selskap":
+    
     input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
 
     if input:
@@ -48,5 +56,18 @@ if filter == "Roller i et selskap":
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
-        st.write(response.json())
+        r = response.json()
+        st.write(r)
+
+        for gruppe in r:
+            st.title(gruppe['type']['beskrivelse'])
+            st.write('Sist endret: ' + gruppe['sistEndret'])
+            for rolle in gruppe['roller']:
+                if 'person' in rolle:
+                    st.subheader('Navn: ' + rolle['person']['navn']['fornavn'] + ' ' + rolle['person']['navn']['etternavn'])
+                    st.write('Rolle: ' + rolle['type']['beskrivelse'])
+                    st.write('Fødselsdato: ' + rolle['person']['fodselsdato'])
+                    
+
+
 
