c6d0f288a8fdc9b7bc6ce8c6da04219ae3c1867f
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-15 17:00:04 +0200

    PEP-sjekk for alle

--- KYC-sjekk.py
+++ KYC-sjekk.py
@@ -3,7 +3,7 @@ import requests
 
 
 
-filter = st.sidebar.selectbox("Søk etter", ["Personer", "Selskap", "Roller i et selskap"])
+filter = st.sidebar.selectbox("Søk etter", ["PEP-sjekk enkeltperson", "Selskap", "Roller i et selskap", "PEP-sjekk for alle i et selskap"])
 st.title(f"Know Your Customer - {filter}")
 
 payload={}
@@ -14,7 +14,7 @@ def PepSøk(input):
     response = requests.request("GET", url, headers=headers, data=payload)
     r = response.json()
     st.write('Number of hits: ' + str(r['numberOfHits']))
-    st.header('Political Exposed Person = ' + str(r['numberOfHits'] > 0))
+    st.subseader('Political Exposed Person = ' + str(r['numberOfHits'] > 0))
     for hit in r["hits"]:
         st.header(hit['name'])
         st.write('BIRTH DATE: ' + hit['birth_date'])
@@ -22,21 +22,17 @@ def PepSøk(input):
         st.write('ALIASES: ' + hit['aliases'])
         st.write('COUNTRY: ' + hit['countries'])
 
-if filter == "Personer":
-   
+if filter == "PEP-sjekk enkeltperson": 
     input = st.text_input("Søk etter person")
     if input:
         PepSøk(input)
     
 
 
-if filter == "Selskap":
-   
+if filter == "Selskap": 
     input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
-
     if input:
         url = "https://stacc-code-challenge-2021.azurewebsites.net/api/enheter?orgNr=" + input
-
         response = requests.request("GET", url, headers=headers, data=payload)
         r = response.json()
         st.header(r['navn'])
@@ -47,18 +43,12 @@ if filter == "Selskap":
         st.write('Forretningsadresse: ' + r['forretningsadresse']['adresse'][0] + ', ' + r['forretningsadresse']['postnummer'] + ', ' + r['forretningsadresse']['poststed'] + ', ' + r['forretningsadresse']['land'])
 
 
-if filter == "Roller i et selskap":
-    
+if filter == "Roller i et selskap":  
     input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
-
     if input:
         url = "https://stacc-code-challenge-2021.azurewebsites.net/api/roller?orgNr=" + input
-
         response = requests.request("GET", url, headers=headers, data=payload)
-
         r = response.json()
-        st.write(r)
-
         for gruppe in r:
             st.title(gruppe['type']['beskrivelse'])
             st.write('Sist endret: ' + gruppe['sistEndret'])
@@ -67,7 +57,26 @@ if filter == "Roller i et selskap":
                     st.subheader('Navn: ' + rolle['person']['navn']['fornavn'] + ' ' + rolle['person']['navn']['etternavn'])
                     st.write('Rolle: ' + rolle['type']['beskrivelse'])
                     st.write('Fødselsdato: ' + rolle['person']['fodselsdato'])
-                    
+            st.markdown("____")
+
+
+if filter == "PEP-sjekk for alle i et selskap":
+    input = st.text_input("Søk etter selskap (bruk organisasjonsnummer)")
+    if input:
+        url = "https://stacc-code-challenge-2021.azurewebsites.net/api/roller?orgNr=" + input
+        response = requests.request("GET", url, headers=headers, data=payload)
+        r = response.json()
+        for gruppe in r:
+            st.title(gruppe['type']['beskrivelse'])
+            st.write('Sist endret: ' + gruppe['sistEndret'])
+            for rolle in gruppe['roller']:
+                if 'person' in rolle:
+                    st.subheader('Navn: ' + rolle['person']['navn']['fornavn'] + ' ' + rolle['person']['navn']['etternavn'])
+                    st.write('Rolle: ' + rolle['type']['beskrivelse'])
+                    st.write('Fødselsdato: ' + rolle['person']['fodselsdato'])
+                    PepSøk(rolle['person']['navn']['fornavn'] + ' ' + rolle['person']['navn']['etternavn'])
+                    st.markdown("____")
+            
 
 
 
