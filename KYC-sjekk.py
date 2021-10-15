5296371c3114c4dc461d0c4261ae7868f89ea890
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-15 10:48:39 +0200

    Lagt til selectbox for valg av hva man vil søke etter

--- KYC-sjekk.py
+++ KYC-sjekk.py
@@ -3,17 +3,41 @@ import requests
 
 st.title("Know Your Customer. PEP-sjekk")
 
-input = st.text_input("Søk etter person")
+filter = st.sidebar.selectbox("Søk etter", ["Personer", "Selskap", "Roller i et selskap"])
 
-if input:
-    url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
+payload={}
+headers = {}
 
-    payload={}
-    headers = {}
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    st.write(response.json())
+if filter == "Personer":
+    input = st.text_input("Søk etter person")
 
 
+    if input:
+        url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        st.write(response.json())
+
+if filter == "Selskap":
+    input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
+
+
+    if input:
+        url = "https://stacc-code-challenge-2021.azurewebsites.net/api/enheter?orgNr=" + input
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        st.write(response.json())
+        
+
+if filter == "Roller i et selskap":
+    input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
+
+    if input:
+        url = "https://stacc-code-challenge-2021.azurewebsites.net/api/roller?orgNr=" + input
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        st.write(response.json())
 
