c1e1529e0a8c5463462d2bc14716aa1dbc5eee72
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-15 14:07:33 +0200

    1. utkast for personsøk

--- KYC-sjekk.py
+++ KYC-sjekk.py
@@ -10,14 +10,23 @@ headers = {}
 
 if filter == "Personer":
     input = st.text_input("Søk etter person")
-
-
     if input:
         url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
 
         response = requests.request("GET", url, headers=headers, data=payload)
+        r = response.json()
+        st.header('Number of hits: ' + str(r['numberOfHits']))
+        for hit in r["hits"]:
+            st.header(hit['name'])
+            st.write('Birth Date: ' + hit['birth_date'])
+            st.write('Found in dataset: \n' + hit['dataset'])
+            st.write('Aliases: \n' + hit['aliases'])
+            st.write('Country: \n' + hit['countries'])
+        st.header('Political Exposed Person = ' + str(r['numberOfHits'] > 0))
 
-        st.write(response.json())
+            
+
+
 
 if filter == "Selskap":
     input = st.text_input("Søk etter Selskap (Bruk Organisasjonsnummer)")
