484d7634f41d3d7cffd9d512dd0675e6fbbf7fa5
hbleie <92336221+hbleie@users.noreply.github.com>
2021-10-15 10:26:16 +0200

    Lagd mulighet for tekstinput

--- KYC-sjekk.py
+++ KYC-sjekk.py
@@ -1,4 +1,19 @@
 import streamlit as st
 import requests
 
+st.title("Know Your Customer. PEP-sjekk")
+
+input = st.text_input("Søk etter person")
+
+if input:
+    url = "https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name=" + input
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    st.write(response.json())
+
+
 
